3086d33aff8d0fa440232fcadaa75aa6040bdbc6
Dean Hutton <huttonjd89@gmail.com>
2015-05-30 19:28:20 -0400

    Modularized the Percent Response Charts
    Modularized the Percent Response Charts

--- GenerateReport.py
+++ GenerateReport.py
@@ -13,19 +13,19 @@ file = open('Employee_Survey.pdf', 'w+')
 
 width, height = letter
 styles = getSampleStyleSheet()
-styleN = styles["BodyText"]
+styleN = styles['BodyText']
 styleN.alignment = TA_LEFT
 
-headerStyle = styles["Normal"]
+headerStyle = styles['Normal']
 headerStyle.alignment = TA_CENTER
 headerStyle.fontSize = 7
-headerStyle.fontName = "Helvetica-Bold"
+headerStyle.fontName = 'Helvetica-Bold'
 
 def coord(x, y, unit=1):
     x, y = x * unit, height -  y * unit
     return x, y
 
-def drawGrid(canvas):
+def draw_grid(canvas):
     GRID_COLOR = (0.5, 0.5, 1)
     INTERVAL = 5 * mm
 
@@ -46,55 +46,85 @@ def drawGrid(canvas):
                voff + numv * INTERVAL, hoff + yi * INTERVAL)
     #c.circle(width / 2, 5 * mm, 0.7, fill=1)
 
-# Headers
+#Create the Header
+def build_percent_responding_rectangles(percentFavorable,percentNeutral,percentUnfavorable,header=False):
+
+    #Header Rectangles
+
+    percentFavorableFloat = float(percentFavorable)/100
+    percentNeutralFloat = float(percentNeutral)/100
+    percentUnfavorableFloat = float(percentUnfavorable)/100
+    if header:
+        d = Drawing(0*mm, 10*mm)
+        totalRectangleLength = 60
+    else:
+        d = Drawing(0*mm, 0*mm)
+        totalRectangleLength = 75
+
+    rectangleHeight = 4
+    rectangleYposition = 0
+    rectangleTextYposition = 1
+    rectangleTextHeight = 2.5
+
+
+    #Favorable rect
+    favorableXposition = 0
+    favorableRectWidth = percentFavorableFloat*totalRectangleLength
+    if header:
+        favorableRectTextXPosition = favorableXposition + 1
+        favorableRectText = '% Favorable'
+    else:
+        favorableRectTextXPosition = (favorableXposition +favorableRectWidth)/2
+        favorableRectText = str(percentFavorable)
+
+    d.add(Rect(favorableXposition*mm, rectangleYposition*mm, favorableRectWidth*mm, rectangleHeight*mm, fillColor=colors.green))
+    d.add(String(favorableRectTextXPosition*mm, rectangleTextYposition*mm, favorableRectText, fontSize=rectangleTextHeight*mm,fontName='Helvetica'))
+
+    #Neutral Rect
+    neutralXposition = favorableRectWidth
+    neutralRectWidth = percentNeutralFloat*totalRectangleLength
+    if header:
+        neutralRectTextXPosition = neutralXposition + 1
+        neutralRectText = '% Neutral'
+    else:
+        neutralRectTextXPosition = (neutralXposition + neutralRectWidth + favorableRectTextXPosition)/2
+        neutralRectText = str(percentNeutral)
+
+    d.add(Rect(neutralXposition*mm, rectangleYposition*mm, neutralRectWidth*mm, rectangleHeight*mm, fillColor=colors.yellow))
+    d.add(String(neutralRectTextXPosition*mm, rectangleTextYposition*mm, neutralRectText, fontSize=rectangleTextHeight*mm,fontName='Helvetica'))
+
+    #Unfavorable Rect
+    unfavorableXposition = neutralRectWidth + favorableRectWidth
+    unfavorableRectWidth = (percentUnfavorableFloat*totalRectangleLength)
+    if header:
+        unfavorableRectTextXPosition = unfavorableXposition + 1
+        unfavorableRectText = '% Unfavorable'
+    else:
+        unfavorableRectTextXPosition = (unfavorableXposition + unfavorableRectWidth + favorableRectTextXPosition + neutralRectTextXPosition)/2
+        unfavorableRectText = str(percentUnfavorable)
+
+    d.add(Rect(unfavorableXposition*mm, rectangleYposition*mm, unfavorableRectWidth*mm, rectangleHeight*mm, fillColor=colors.red))
+    d.add(String(unfavorableRectTextXPosition*mm,rectangleTextYposition*mm, unfavorableRectText, fontSize=rectangleTextHeight*mm,fontName='Helvetica'))
+
+    if header:
+        #Percent Responding Heading text
+        percentResponseTextXposition = 15
+        percentResponseTextYposition = 5
+        percentResponseText = 'Percent Responding'
+        d.add(String(percentResponseTextXposition*mm,percentResponseTextYposition*mm, percentResponseText, fontSize=7, fontName='Helvetica-Bold'))
+    return d
+
+
+percentRespondingHeader = build_percent_responding_rectangles(33, 33, 33, header=True)
+chart = build_percent_responding_rectangles(20, 40, 20, header=False)
 perfMgmtHeader = Paragraph('''<b>Perfomance Managment</b>''', headerStyle)
 totalNheader = Paragraph('''<b>Total N''', headerStyle)
 percentResp = Paragraph('''<b>Percent Responding''', headerStyle)
 percentFavHeader = Paragraph('''<b>% Fav''', headerStyle)
 percentDistHeader = Paragraph('''<b>% Distribution''', headerStyle)
 meanHeader = Paragraph('''<b>Mean''', headerStyle)
-
-#Percent Responding Header
-#Header Rectangles
-d = Drawing(0*mm, 10*mm)
-rectangleHeight = 4
-rectangleYposition = 0
-rectangleTextYposition = 1
-rectangleTextHeight = 3
-
-#Favorable rect
-favorableXposition = 0
-favorableRectWidth = 20
-favorableRectTextXPosition = favorableXposition + 1 #(favorableXposition +favorableRectWidth)/2
-favorableRectText = "% Favorable"
-d.add(Rect(favorableXposition*mm, rectangleYposition*mm, favorableRectWidth*mm, rectangleHeight*mm, fillColor=colors.green))
-d.add(String(favorableRectTextXPosition*mm, rectangleTextYposition*mm, favorableRectText, fontSize=2.5*mm,fontName="Helvetica"))
-
-#Neutral Rect
-neutralXposition = 20
-neutralRectWidth = 20
-neutralRectTextXPosition = neutralXposition + 1 #(neutralXposition + neutralRectWidth + favorableRectTextXPosition)/2
-neutralRectText = "% Neutral"
-d.add(Rect(neutralXposition*mm, rectangleYposition*mm, neutralRectWidth*mm, rectangleHeight*mm, fillColor=colors.yellow))
-d.add(String(neutralRectTextXPosition*mm, rectangleTextYposition*mm, neutralRectText, fontSize=2.5*mm,fontName="Helvetica"))
-
-#Unfavorable Rect
-unfavorableXposition = 40
-unfavorableRectWidth = 20
-unfavorableRectTextXPosition = unfavorableXposition + 1#(unfavorableXposition + unfavorableRectWidth + favorableRectTextXPosition + neutralRectTextXPosition)/2
-unfavorableRectText = "% Unfavorable"
-d.add(Rect(unfavorableXposition*mm, rectangleYposition*mm, unfavorableRectWidth*mm, rectangleHeight*mm, fillColor=colors.red))
-d.add(String(unfavorableRectTextXPosition*mm,rectangleTextYposition*mm, unfavorableRectText, fontSize=2.5*mm,fontName="Helvetica"))
-
-#Percent Responding Heading text
-percentResponseTextXposition = 15
-percentResponseTextYposition = 5
-percentResponseText = 'Percent Responding'
-d.add(String(percentResponseTextXposition*mm,percentResponseTextYposition*mm, percentResponseText, fontSize=7,fontName="Helvetica-Bold"))
-
-
-data= [[perfMgmtHeader, totalNheader,d, percentFavHeader, percentDistHeader,meanHeader],
-       ["10", '11', "12", '13', '14', '15'],
+data= [[perfMgmtHeader, totalNheader,percentRespondingHeader, percentFavHeader, percentDistHeader,meanHeader],
+       ['10', '11', chart, '13', '14', '15'],
        ['20', '21', '22', '23', '24', '25'],
        ['30', '31', '32', '33', '34', '35'],
        ['40', '41', '42', '43', '44', '45'],
@@ -121,7 +151,7 @@ t._argW[5] = 10.922*mm # Mean
 c = canvas.Canvas(buffer, pagesize=letter)
 t.wrapOn(c, width, height)
 t.drawOn(c, *coord(5, 60, mm))
-drawGrid(c)
+#drawGrid(c)
 c.save()
 
 #Write the buffer to disk and close the file
