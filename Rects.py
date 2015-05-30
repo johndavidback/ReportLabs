deab5d908f806046306da87283edfe855bf2ad7a
Dean Hutton <huttonjd89@gmail.com>
2015-05-29 22:14:24 -0400

    Added a Grid Function
    Added grid function that can be turned on/off with set intervals of mm.
    Right now I am using 5mm but it can be changed to w/e. Very useful for
    debugging and lining everything up perfectly

--- Rects.py
+++ Rects.py
@@ -1,14 +1,24 @@
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4, cm, letter, inch
+from reportlab.lib.pagesizes import A4, cm
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph, Table, TableStyle
 from reportlab.lib.enums import TA_JUSTIFY, TA_LEFT, TA_CENTER
 from reportlab.lib import colors
-from reportlab.lib.units import mm
+from reportlab.lib.pagesizes import letter, inch
 from io import BytesIO
 
+def variousshapes(canvas):
+     from reportlab.lib.units import inch
+     inch = int(inch)
+     canvas.setStrokeGray(0.5)
+     canvas.grid(range(0,int(11*inch/2),int(inch/2)), range(0,int(7*inch/2),int(inch/2)))
+     canvas.setLineWidth(4)
+     canvas.setStrokeColorRGB(0, 0.2, 0.7)
+     canvas.setFillColorRGB(1, 0.6, 0.8)
+     canvas.rect(inch,inch,6*inch,9*inch, fill=1)
+
 buffer = BytesIO()
-file = open('Employee_Survey.pdf', 'w+')
+file = open('rects.pdf', 'w+')
 
 width, height = letter
 styles = getSampleStyleSheet()
@@ -22,28 +32,6 @@ styleBH.fontSize = 7
 def coord(x, y, unit=1):
     x, y = x * unit, height -  y * unit
     return x, y
-
-def drawGrid(canvas):
-    GRID_COLOR = (0.5, 0.5, 1)
-    INTERVAL = 5 * mm
-
-    numv = int((width - ((4 + 4) * mm)) / INTERVAL)
-    numh = int((height - (16 + 3) * mm) / INTERVAL)
-    voff = (width - numv * INTERVAL) / 2.0
-    hoff = 16 * mm
-
-    # Draw
-    c = canvas
-    c.setLineWidth(0.15)
-    c.setStrokeColorRGB(*GRID_COLOR)
-    for xi in xrange(numv + 1):
-        c.line(xi * INTERVAL + voff, hoff,
-               xi * INTERVAL + voff, hoff + numh * INTERVAL)
-    for yi in xrange(numh + 1):
-        c.line(voff, hoff + yi * INTERVAL,
-               voff + numv * INTERVAL, hoff + yi * INTERVAL)
-    #c.circle(width / 2, 5 * mm, 0.7, fill=1)
-    c.save()
 
 # Headers
 perfMgmtHeader = Paragraph('''<b>Perfomance Managment</b>''', styleBH)
@@ -74,9 +62,10 @@ t._argW[4] = 1.5*inch # % Dist
 t._argW[5] = .43*inch # Mean
 
 c = canvas.Canvas(buffer, pagesize=A4)
+variousshapes(c)
 t.wrapOn(c, width, height)
-t.drawOn(c, *coord(5, 30, mm))
-drawGrid(c)
+t.drawOn(c, *coord(1, 3, cm))
+c.save()
 
 #Write the buffer to disk and close the file
 pdf = buffer.getvalue()
